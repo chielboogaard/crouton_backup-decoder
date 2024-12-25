2e255e565c69598b16b6d3f4a11eeb4bc916fb50
Mrityunjayyshukla <shuklamrityunjay60@gmail.com>
2024-12-25 20:48:37 +0530

    fixes in convert file

--- convert.py
+++ convert.py
@@ -1,3 +1,4 @@
+# Use the data from the crouton file to generate a PDF file with the recipe information
 from zip_class import ZipHandler
 from crouton_decode_class import CroutonDecodeHandler
 from pdf_class import RecipePDFHandler
@@ -5,22 +6,28 @@ from pdf_class import RecipePDFHandler
 import json
 import os
 
-FILEPATH = "Crouton Recipes - example"
+# Define the file paths for the ZIP file and the output folder
+FILEPATH = "Crouton_Recipes"
+if not os.path.exists(FILEPATH):
+    raise FileNotFoundError(f"File not found: {FILEPATH}")
 OUTPUTPATH = "output"
 
 
 def main():
+    # Create a ZipHandler instance and extract the crouton files
     zip = ZipHandler(zip_file_path=FILEPATH)
-
+    # Extract the crouton files from the ZIP file
     croutons = zip.extract_croutonFiles()
-
+    # Create the output folder if it doesn't exist
     os.makedirs(OUTPUTPATH, exist_ok=True)
 
+    # Iterate over the crouton files and process each recipe
     for recipe in croutons:
+        # used to decode the JSON object representing a recipe
         crouton = CroutonDecodeHandler(croutonFile=json.loads(recipe))
-
+        # Extract the recipe information
         recipeInfo = crouton.get_recipeInfo()
-
+        # Extract the recipe ingredients and instructions
         recipeData = {
                 "title": recipeInfo.get("name"),
                 "serves": recipeInfo.get("serves"),
@@ -29,6 +36,7 @@ def main():
                 "instructions": crouton.get_instructions(),
             }
         
+        # Generate a PDF file for the recipe
         pdf = RecipePDFHandler(recipeData, OUTPUTPATH, f"{recipeInfo.get("name")}.pdf")
         pdf.generate_pdf()
         
